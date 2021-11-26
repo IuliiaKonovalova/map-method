08e718dffe216b46144096079c64b1bf2b312a6c
Iuliia_Konovalova <JuliiaKonn@protonmail.com>
2021-11-26 05:19:49 +0000

    Solve map method challenges

--- map.js
+++ map.js
@@ -6,14 +6,35 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+
+for (let num of nums) {
+  results.push(num * 2);
+}
+
+console.log(results);
 
 // Using map()
 
+const multByTwo = (num) => {
+  return num * 2;
+}
+
+const mapResults = nums.map(multByTwo);
+console.log(multByTwo);
 
 // Simplified w/ map()
 
+const simplified = nums.map(function (num) {return num * 2});
+
+console.log(simplified)
 
 // Simplfied w/ map() + arrow function
+
+const simplyWithArrowFunction = nums.map(num =>  num * 2)
+
+console.log(simplyWithArrowFunction)
 
 
 // With objects:
@@ -37,3 +58,7 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentId = students.map(student => [student.name, student.id])
+
+console.log(studentId)
